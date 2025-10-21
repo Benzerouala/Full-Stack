7801da5a5a4723923fd278a2e42c78f65ad70b13
Benzerouala <benzeroualaossama@gmail.com>
2025-10-21 11:42:08 +0100

    Update project: deleted old files and updated node.js

--- mathB.js
+++
@@ -1,5 +0,0 @@
-const PI = 3.14;
-function calculeAire(rayon){
-    return PI*rayon*rayon
-}
-console.log(calculeAire(2));
--- utilities/greet.js
+++
@@ -1,8 +0,0 @@
-function saluer(nom){
-    return `Bonjour,${nom}`
-}
-function direAuRevoir(nom){
-    return `Au revoir, ${nom} !`;
-}
-module.exports = {saluer,direAuRevoir};
-
